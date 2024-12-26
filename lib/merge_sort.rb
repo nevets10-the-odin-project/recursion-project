cec8fc178d458dc0c4b32274abba07e13ed09e44
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2024-12-26 14:36:37 -0800

    Implement merge_sort.rb method

--- lib/merge_sort.rb
+++ lib/merge_sort.rb
@@ -1,2 +1,28 @@
 def merge_sort(arr)
+  return arr if arr.length == 1
+
+  half = (arr.length - 1) / 2
+
+  first_half = merge_sort(arr[..half])
+  second_half = merge_sort(arr[(half + 1)..])
+  new_arr = []
+
+  while first_half.any? && second_half.any?
+    new_arr << if first_half[0] <= second_half[0]
+                 first_half.shift
+               else
+                 second_half.shift
+               end
+  end
+
+  new_arr << if first_half.any?
+               first_half
+             else
+               second_half
+             end
+
+  new_arr.flatten
 end
+
+p merge_sort([3, 2, 1, 13, 8, 5, 0, 1])
+p merge_sort([105, 79, 100, 110])
